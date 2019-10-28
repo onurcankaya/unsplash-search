88ecd347ee50c1dd77a98da7f2fe5c6141cc06f9
onurcankaya <onur.eren.cankaya@gmail.com>
2019-10-28 06:45:34 +0100

    implmeneted some grids for the image results

--- src/components/image-list.js
+++ src/components/image-list.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import Image from './image'
+import ImageCard from './image'
 
-const ImageListContainer = styled.div``
+const ImageListContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(270px, 1fr));
+  grid-gap: 0 50px;
+  grid-auto-rows: 10px;
+`
 
 class ImageList extends React.PureComponent {
   render() {
@@ -12,7 +17,7 @@ class ImageList extends React.PureComponent {
     return (
       <ImageListContainer>
         {images.map((image) => (
-          <Image key={image.id} url={image.url} alt={image.alt} />
+          <ImageCard key={image.id} url={image.url} alt={image.alt} />
         ))}
       </ImageListContainer>
     )
--- src/components/image.js
+++ src/components/image.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const ImageContainer = styled.div``
-const Img = styled.img`
-  width: 400px;
-  height: auto;
+const ImageContainer = styled.div`
+  grid-row-end: ${(props) => `span ${props.spans}`};
+`
+const Image = styled.img`
+  width: 270px;
+  grid-row-end: span 2;
 `
 
-const Image = ({ id, url, alt }) => (
-  <ImageContainer key={id}>
-    <Img src={url} alt={alt} />
-  </ImageContainer>
-)
+class ImageCard extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = { spans: 0 }
 
-export default Image
+    this.imageRef = React.createRef()
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans)
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight
+    const spans = Math.ceil(height / 10)
+
+    this.setState({ spans })
+  }
+
+  render() {
+    const { id, url, alt } = this.props
+
+    return (
+      <ImageContainer key={id} spans={this.state.spans}>
+        <Image ref={this.imageRef} src={url} alt={alt} />
+      </ImageContainer>
+    )
+  }
+}
+
+export default ImageCard
