747a412689975dcd0b052628a91be1e07a1933c4
onurcankaya <onur.eren.cankaya@gmail.com>
2020-01-16 10:59:56 +0100

    added text for empty state

--- src/components/image-list.js
+++ src/components/image-list.js
@@ -9,17 +9,24 @@ const ImageListContainer = styled.div`
   grid-gap: 0 10px;
   grid-auto-rows: 10px;
 `
+const NoResult = styled.div`
+  padding: 20px 10px;
+  font-size: 16px;
+`
 
 class ImageList extends React.PureComponent {
   render() {
     const { images } = this.props
 
+    console.log(images)
+
     return (
       <ImageListContainer>
-        {images &&
-          images.map((image) => (
-            <ImageCard key={image.id} url={image.url} alt={image.alt} />
-          ))}
+        {images.length > 0 ? (
+          images.map((image) => <ImageCard key={image.id} url={image.url} alt={image.alt} />)
+        ) : (
+          <NoResult>No images to show yet...</NoResult>
+        )}
       </ImageListContainer>
     )
   }
